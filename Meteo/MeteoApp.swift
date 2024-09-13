6f6894b5fe00ac1a8eceb2f54695a897598b087e
Théo B <t.blanchard@gmail.com>
2024-09-13 10:00:37 +0200

    Initial Commit

---
+++ Meteo/MeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoApp.swift
+//  Meteo
+//
+//  Created by Theo Blanchard on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
