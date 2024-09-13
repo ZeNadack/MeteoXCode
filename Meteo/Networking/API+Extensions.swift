49398be92da886c7e000d0b370578675b41059a1
ZeNadack <t.blanchard52000@gmail.com>
2024-09-13 17:20:31 +0200

    ajout du dossier Networking et des classes API et API+Extensions

---
+++ Meteo/Networking/API+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  API+Extensions.swift
+//  Meteo
+//
+//  Created by Theo Blanchard on 9/13/24.
+//
+
+import Foundation
+
+//classe crée afin de cacher la clé API
+
+extension API {
+    static let baseURLString = "https://api.openweathermap.org/data/3.0"
+    
+    static func getURLfor(lat: Double, lon: Double) -> String {
+        return"\(baseURLString)oncall,lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial"
+    }
+}
---
+++ Meteo/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  Meteo
+//
+//  Created by Theo Blanchard on 9/13/24.
+//
+
+import Foundation
+
+struct API {
+    static let key = "16c5ca662b13a22d3897b198031492a9"
+}
