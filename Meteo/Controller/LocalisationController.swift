fefd2974b1991bf7d5427ffc8bf879d482a9bb24
ZeNadack <t.blanchard52000@gmail.com>
2024-09-13 10:51:56 +0200

    Ajout du dossier Controller et du fichier LocalisationController

---
+++ Meteo/Controller/LocalisationController.swift
@@ -0,0 +1,11 @@
+//
+//  LocalisationController.swift
+//  Meteo
+//
+//  Created by Theo Blanchard on 9/13/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocalisationController: NSObject, ObservableObject, CLLocationManagerDelegate 
