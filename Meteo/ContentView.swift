aeeef31d958af86147166b96e22a9779fe58c296
ZeNadack <t.blanchard52000@gmail.com>
2024-09-27 11:11:12 +0200

    Modification du fichier ContentView

--- Meteo/ContentView.swift
+++ Meteo/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var cityVM = CityViewViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(alignment: .bottom) {
+            VStack(spacing: 0) {
+                MenuHeaderView(cityVM: cityVM)
+                ScrollView(showsIndicators: false) {
+                    CityView(cityVM: cityVM)
+                }
+            }.padding(.top, 30)
+        }.background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5388317108, green: 0.7777397037, blue: 1, alpha: 1)), Color[#colorLiteral(red: 0.2366411984, green: 0.6955710053, blue: 1, alpha: 1)]), startPoint: .topLeading, endPoint: .bottomLeading))
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
