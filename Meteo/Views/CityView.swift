de67bd2796abeca8fdec4afa2bfc0b3228243e69
ZeNadack <t.blanchard52000@gmail.com>
2024-09-28 09:22:40 +0200

    Création et Modification de la View CityView

---
+++ Meteo/Views/CityView.swift
@@ -0,0 +1,30 @@
+//
+//  CityView.swift
+//  Meteo
+//
+//  Created by Theo Blanchard on 9/28/24.
+//
+
+import Foundation
+
+struct CityView: View {
+    
+    @ObservedObject var cityVM: CityViewViewModel
+    
+    var body: some View {
+        VStack {
+            CityNameView(city: cityVM.city, date: cityVM.date)
+                .shadow(radius: 0)
+            TodayWeatherView(cityVM: cityVM)
+                .padding()
+            HourlyWeatherView(cityVM: cityVM)
+            DailyWeatherView(cityVM: cityVM)
+        }.padding(.bottom, 30)
+    }
+}
+
+struct CityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
